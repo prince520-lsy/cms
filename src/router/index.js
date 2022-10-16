dcf67c6be83291e722c8333a895f71c5747dd07c
jinjinliang <3122702689@qq.com>
2022-10-16 10:58:32 +0800

    cms-init

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,17 @@
+
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/login',
+    name: 'login',
+    //按需加载，只有访问到页面才会加载
+    component: () => import('@/views/login/index.vue')
+  },
+
 ]
 
 const router = new VueRouter({
