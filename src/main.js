fa5762dd1e72cc4745faa48e127153031c87c0b0
jinjinliang <3122702689@qq.com>
2022-10-18 21:01:10 +0800

    nav-protect-finished

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue';
 import router from './router'
 import store from './store'
+import './permissoin'
 Vue.use(ElementUI);
 
 new Vue({
